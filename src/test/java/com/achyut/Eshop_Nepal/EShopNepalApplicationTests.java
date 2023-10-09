f1feef278a629bfefade3a9bbcf856d9718d7204
Achyut Thapa <achyutthapa6@gmail.com>
2023-10-09 16:06:31 +0545

    started with Eshop_Nepal

---
+++ src/test/java/com/achyut/Eshop_Nepal/EShopNepalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achyut.Eshop_Nepal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopNepalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
